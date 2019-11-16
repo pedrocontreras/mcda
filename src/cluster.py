9c275ce29f0e5b0e51fc75e3a2e78d5fbb986d2c
xjavierpereira7 <xjavierpereira7@gmail.com>
2019-11-16 09:45:02 -0300

    fixup! mira el comentario

--- src/cluster.py
+++ src/cluster.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+def aweonao():
+    print   ('gil')
+
+
+
 def k_means(categoria, actions, limites):
     n_acc = np.size(actions, 0)  # number of acciones
     n_cri = np.size(actions, 1)  # number if criteria
